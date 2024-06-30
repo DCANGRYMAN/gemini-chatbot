871068e676b925b588f3e7d8d59dec883e89624c
Darlan <darlandc@gmail.com>
2024-06-30 03:10:08 -0300

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,44 @@
-import { Component } from '@angular/core';
-
+import { CommonModule } from '@angular/common';
+import { Component, inject } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { RouterOutlet } from '@angular/router';
+import { GeminiService } from './services/gemini.service'
+import { SkeletonComponent } from './skeleton/skeleton.component';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'vito-app';
+  title = 'gemini-inte';
+
+  prompt: string = '';
+
+  geminiService: GeminiService = inject(GeminiService);
+
+  loading: boolean = false;
+
+  chatHistory: any[] = [];
+  constructor() {
+    this.geminiService.getMessageHistory().subscribe((res: any) => {
+      if(res) {
+        this.chatHistory.push(res);
+      }
+    })
+  }
+
+  async sendData() {
+    if(this.prompt && !this.loading) {
+      this.loading = true;
+      const data = this.prompt;
+      this.prompt = '';
+      await this.geminiService.generateText(data);
+      this.loading = false;
+    }
+  }
+
+  formatText(text: string) {
+    const result = text.replaceAll('*', '');
+    return result;
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,29 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { GeminiService } from './services/gemini.service';
+import { SkeletonComponent } from './skeleton/skeleton.component';
+import { CommonModule } from '@angular/common';
+import { RouterOutlet } from '@angular/router';
+
+import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent, SkeletonComponent],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    FormsModule,
+    HttpClientModule,
+    RouterOutlet,
+    CommonModule,
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  exports: [AppComponent, SkeletonComponent],
+  providers: [GeminiService],
+  bootstrap: [AppComponent],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
 })
-export class AppModule { }
+export class AppModule {}
+
